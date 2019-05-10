08d723d778ddf342d999cd02ca165df56d235b77
Emilian Ćwiok <emilian.cw@gmail.com>
2019-05-10 12:44:10 +0200

    Added cors

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const app = express();
 require('dotenv').config();
@@ -10,6 +11,7 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
 // Middlewares
 app.use(morgan('dev'));
 app.use(bodyParser.json());
+app.use(cors());
 
 // Routes
 
