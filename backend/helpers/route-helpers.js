465fa495a2fa4f2efd743691a7d872d8fd54ea12
Emilian Ćwiok <emilian.cw@gmail.com>
2019-05-10 21:25:13 +0200

    remove joi requirments

--- backend/helpers/route-helpers.js
+++ backend/helpers/route-helpers.js
@@ -20,11 +20,11 @@ module.exports = {
         signUpSchema: Joi.object().keys({
             name: Joi.string().min(3).required(),
             email: Joi.string().email().required(),
-            password: Joi.string().min(6).max(16).required()
+            password: Joi.string().min(6).required()
         }),
         signInSchema: Joi.object().keys({
             email: Joi.string().email().required(),
-            password: Joi.string().min(6).max(16).required()
+            password: Joi.string().min(6).required()
         })
     }
 }
