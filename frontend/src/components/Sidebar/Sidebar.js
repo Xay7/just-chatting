f9827665d4fc45c09df24d8283ea00dd894c879d
Emilian Ćwiok <emilian.cw@gmail.com>
2019-05-12 16:16:17 +0200

    Added sidebar users boilerplate

---
+++ frontend/src/components/Sidebar/Sidebar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Sidebar.module.scss';
+
+const Sidebar = props => {
+    return (
+        <div className={styles.Sidebar}>Hello</div>
+    )
+}
+
+export default Sidebar;
---
+++ frontend/src/components/Users/Users.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Users.module.scss';
+
+const Users = (props) => {
+    return (
+        <div className={styles.Users}>Chatbox</div>
+    )
+}
+
+export default Users;
