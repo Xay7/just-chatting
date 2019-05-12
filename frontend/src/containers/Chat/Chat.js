b799966c42a9f71711d09af42f613994c964ed06
Emilian Ćwiok <emilian.cw@gmail.com>
2019-05-12 16:17:26 +0200

    Added sidebar, chatbox, users

--- frontend/src/containers/Chat/Chat.js
+++ frontend/src/containers/Chat/Chat.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Sidebar from '../../components/Sidebar/Sidebar';
+import Chatbox from '../Chatbox/Chatbox';
+import Users from '../../components/Users/Users';
+import styles from './Chat.module.scss';
 
 class Chat extends Component {
     render() {
         return (
-            <div>Secret </div>
+            <div className={styles.Chat}>
+                <Sidebar />
+                <Chatbox />
+                <Users />
+            </div>
         )
     }
 }
