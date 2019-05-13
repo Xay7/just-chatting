9c72f0c3e25930641967272baf6f3c64cf9bc95a
Emilian Ćwiok <emilian.cw@gmail.com>
2019-05-13 18:27:00 +0200

    Added socket export for components

--- frontend/src/containers/Chat/Chat.js
+++ frontend/src/containers/Chat/Chat.js
@@ -1,20 +1,26 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 import Sidebar from '../../components/Sidebar/Sidebar';
 import Chatbox from '../Chatbox/Chatbox';
-import Users from '../../components/Users/Users';
+import Users from '../Users/Users';
 import styles from './Chat.module.scss';
+import io from 'socket.io-client';
+
+export const socketChat = io('http://localhost:3001');
 
 class Chat extends Component {
+
     render() {
         return (
             <div className={styles.Chat}>
                 <Sidebar />
                 <Chatbox />
                 <Users />
+
             </div>
         )
     }
 }
 
-export default connect(null)(Chat);
+
+
+export default Chat;
