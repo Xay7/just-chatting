01f3a29c1265db4af4873b06b27522eba388cc05
Emilian Ćwiok <emilian.cw@gmail.com>
2019-05-16 20:39:42 +0200

    Added chatroom schema

---
+++ backend/models/chatroom.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const chatSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    owner: {
+        type: String,
+        required: true
+    }
+
+})
+
+const chatroom = mongoose.model('chatroom', chatSchema);
+
+module.exports = chatroom;
