0c68e67074242482dba92940004b64d6a5b841d1
Emilian Ćwiok <emilian.cw@gmail.com>
2019-05-16 20:38:06 +0200

    Added newchat, updated chat controller

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,5 +1,6 @@
 const JWT = require('jsonwebtoken');
 const User = require('../models/user');
+const Chatroom = require('../models/chatroom');
 const { JWT_S } = require('../config/index');
 
 signToken = user => {
@@ -53,9 +54,48 @@ module.exports = {
         res.status(200).json({ token, name: foundUser.local.name, users: io.users });
     },
     chat: async (req, res, next) => {
-        res.status(200).json({ secret: "success", name: req.user.local.name })
-    },
 
+        const name = req.user.local.name
+
+        const findUser = await User.findOne({ "local.name": name })
+
+        res.status(200).json({ name: findUser.local.name, chatRooms: findUser.local.chatRooms });
+    },
+    newChat: async (req, res, next) => {
+
+
+        const name = req.body.name
+        const owner = req.body.owner
+
+
+        const newChatroom = new Chatroom({
+            name: name,
+            owner: owner,
+        })
+
+        const foundChatroom = await Chatroom.findOne({ "name": name });
+
+        if (foundChatroom) {
+            return res.status(403).send({ error: "Chatroom name arleady in use" });
+        }
+
+        await newChatroom.save();
+
+        // Push chatroom to user subscribed rooms   
+        // await User.findOne({ "local.name": owner }, function (err, doc) {
+        //     doc.local.chatRooms.push(name);
+        //     doc.save({ validateBeforeSave: false });
+        // })
+
+        await User.findOneAndUpdate({ "local.name": owner }, {
+            "$push":
+            {
+                "local.chatRooms": name
+            }
+        })
+
+        res.status(200).json({ success: "New chat has been created" })
+    },
     googleOAuth: async (req, res, next) => {
         const token = signToken(req.user);
         res.status(200).json({ token });
