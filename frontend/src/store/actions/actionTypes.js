b44d818e3198dfa11e0d247818470164a7e8237d
Emilian Ćwiok <emilian.cw@gmail.com>
2019-05-23 15:55:22 +0200

    Added new action types

--- frontend/src/store/actions/actionTypes.js
+++ frontend/src/store/actions/actionTypes.js
@@ -1,3 +1,4 @@
+// AUTH
 export const AUTH_SIGN_IN = 'AUTH_SIGN_IN';
 export const AUTH_SIGN_UP = 'AUTH_SIGN_UP';
 export const AUTH_SIGN_IN_ERROR = 'AUTH_SIGN_IN_ERROR';
@@ -5,5 +6,10 @@ export const AUTH_SIGN_UP_ERROR = 'AUTH_SIGN_UP_ERROR';
 export const AUTH_ERROR = 'AUTH_ERROR';
 export const TOKEN_ACCESS = 'TOKEN_ACCESS';
 export const TOKEN_ERROR = 'TOKEN_ERROR';
+
+// CHAT
+export const GET_ROOMS = 'GET_ROOMS';
+export const NEW_ROOM = 'NEW_ROOM';
 export const CHANGE_ROOM = 'CHANGE_ROOM';
+export const JOIN_ROOM = 'JOIN_ROOM';
 export const DELETE_ROOM = 'DELETE_ROOM';
