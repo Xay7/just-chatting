f6ed21f7136750766bffba49abe80a489ec2fff7
Emilian Ćwiok <emilian.cw@gmail.com>
2019-05-31 23:09:24 +0200

    Add fontawesome icon

--- frontend/src/containers/RoomHelpers/RoomHelpers.js
+++ frontend/src/containers/RoomHelpers/RoomHelpers.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
 import styles from './RoomHelpers.module.scss';
 import { connect } from 'react-redux';
-
+import * as actions from '../../store/actions/chatroom';
 class RoomHelpers extends Component {
     render() {
         return (
-            <div className={styles.RoomHelpers}>
-                <div className={styles.Room}>
-                    <h2 className={styles.RoomName}>{this.props.roomName}</h2>
-                    <button className={styles.RoomSettings}>OPTIONS</button>
+            <React.Fragment>
+                <div className={styles.RoomHelpers} >
+                    <div className={styles.Room} onClick={this.props.showRoomOptions}>
+                        <h2 className={styles.RoomName}>{this.props.roomName}</h2>
+                        <i
+                            className="fas fa-caret-down fa-lg"
+                            style={{
+                                color: "white",
+                                marginRight: '22px'
+                            }}></i>
+                    </div>
+                    <div className={styles.Channel}>
+                        <h2 className={styles.ChannelName}>{this.props.channelName}</h2>
+                    </div>
                 </div>
-                <div className={styles.Channel}>
-                    <h2 className={styles.ChannelName}>{this.props.channelName}</h2>
-                </div>
-            </div>
+            </React.Fragment>
         )
     }
 }
@@ -25,4 +32,4 @@ const mapStateToProps = state => {
     }
 }
 
-export default connect(mapStateToProps)(RoomHelpers);
+export default connect(mapStateToProps, actions)(RoomHelpers);
