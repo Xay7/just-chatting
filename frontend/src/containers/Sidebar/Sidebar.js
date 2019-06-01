99731ac5abe422ccc7aab9143c850ae3a35d5ff9
Emilian Ćwiok <emilian.cw@gmail.com>
2019-06-01 21:56:41 +0200

    Add room options

--- frontend/src/containers/Sidebar/Sidebar.js
+++ frontend/src/containers/Sidebar/Sidebar.js
@@ -3,6 +3,9 @@ import styles from './Sidebar.module.scss';
 import { connect } from 'react-redux';
 import * as actions from '../../store/actions/chatroom';
 import uuid4 from 'uuid4'
+import { Spring } from 'react-spring/renderprops';
+import Confirm from '../../components/Confirm/Confirm';
+import Modal from '../../components/Modal/Modal';
 
 class Sidebar extends Component {
 
@@ -10,6 +13,7 @@ class Sidebar extends Component {
         super(props);
         this.state = {
             selectedChannel: '',
+            showDeleteBox: false
         }
 
         this.socket = this.props.socketChat;
@@ -17,7 +21,6 @@ class Sidebar extends Component {
         this.switchChannel = async (id, name) => {
 
             await this.setState({ selectedChannel: id });
-
             let data = {
                 channelID: id,
                 roomID: this.props.roomID,
@@ -26,12 +29,11 @@ class Sidebar extends Component {
 
             await this.props.changeChannel(id, name);
             await this.props.getChatMessages(data);
-            await this.socket.emit('SWITCH_ROOMS', {
+            await this.socket.emit('JOIN_CHANNEL', {
                 room: id,
                 name: this.props.username
             })
         }
-
     }
 
     addChannel = async () => {
@@ -52,6 +54,10 @@ class Sidebar extends Component {
 
     }
 
+    deleteRoomHandler = () => {
+        this.setState({ showDeleteBox: true })
+    }
+
     deleteRoom = async (id, username) => {
 
         const data = {
@@ -61,41 +67,98 @@ class Sidebar extends Component {
 
         await this.props.deleteRoom(data);
 
-        this.setState({ chatRooms: this.props.chatRooms });
-
     }
 
 
     currentChannelStyle = (index) => {
-        const isSelected = this.state.selectedChannel === index;
-        return isSelected ? "red" : "black";
+        const isSelected = this.props.channelID === index;
+        return isSelected ? styles.ChannelSelected : styles.Channel
     }
 
     currentChannelDisable = (index) => {
-        const isSelected = this.state.selectedChannel === index;
+        const isSelected = this.props.channelID === index;
         return isSelected ? true : false;
+    }
+
+    hideDeleteBox = () => {
+        this.setState({ showDeleteBox: false })
     }
 
 
     render() {
 
         let channels = this.props.channels.map(el => {
-            return <div style={{ color: "white" }} key={el.id}>
+            return <div style={{ color: "white" }} key={el.id} className={styles.ChannelWrapper}>
                 <button
-                    style={{ color: this.currentChannelStyle(el.id) }}
                     onClick={() => this.switchChannel(el.id, el.name)}
                     disabled={this.currentChannelDisable(el.id)}
-                >{el.name}</button>
+                    className={this.currentChannelStyle(el.id)}
+                >{"# " + el.name}</button>
             </div>
-        })
+        });
+
+        let roomOptions = this.props.showOptions &&
+            <Spring
+                config={{
+                    duration: 150
+                }}
+                from={{
+                    height: 0
+                }}
+                to={{
+                    height: 80
+                }}>
+                {props => (
+                    <div className={styles.Options} style={props}>
+                        <div className={styles.OptionsBtn}>
+                            <div className={styles.IconsWrapper}>
+                                <i className="fas fa-user-plus "></i>
+                            </div>
+                            <div className={styles.OptionsDescription}>
+                                Invite people
+                            </div>
+                        </div>
+                        <div className={styles.OptionsBtnRed} onClick={this.deleteRoomHandler}>
+                            <div className={styles.IconsWrapper}>
+                                <i className="fas fa-trash-alt "></i>
+                            </div>
+                            <div className={styles.OptionsDescription}>
+                                Delete room
+                            </div>
+
+                        </div>
+                    </div>
+                )}
+
+
+            </Spring>;
+
+
 
 
         return (
-            <div className={styles.Sidebar}>
-                <button onClick={this.joinRoom}>JOIN ROOM</button>
-                <button onClick={this.addChannel}>NEW CHANNEL</button>
-                {channels}
-            </div>
+            <React.Fragment>
+                {this.state.showDeleteBox ?
+                    <div>
+                        <Modal onclick={this.hideDeleteBox} />
+                        <Confirm
+                            cancel={this.hideDeleteBox}
+                            confirm={() => this.deleteRoom(this.props.roomID, this.props.username)}
+                        />
+                    </div> : null}
+                <div className={styles.Sidebar}>
+                    {roomOptions}
+
+                    <div className={styles.Channels}>
+                        <div className={styles.ChannelsHeader}>
+                            <h3 className={styles.ChannelTitle}>Channels</h3>
+                            <button onClick={this.addChannel} className={styles.AddChannel}>+</button>
+                        </div>
+                        {channels}
+
+                    </div>
+                </div>
+            </React.Fragment>
         )
     }
 
@@ -107,7 +170,10 @@ const mapStateToProps = state => {
         username: state.auth.username,
         socketChat: state.auth.socket,
         roomID: state.chat.roomID,
-        channels: state.chat.channels
+        channels: state.chat.channels,
+        roomName: state.chat.roomName,
+        showOptions: state.chat.showRoomOptions,
+        channelID: state.chat.channelID
     }
 
 }
