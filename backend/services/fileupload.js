28542def5d4cb3d9831795df9bb663e8fd732f55
Emilian Ćwiok <emilian.cw@gmail.com>
2019-06-04 12:39:31 +0200

    Add file uploading to s3 bucket

---
+++ backend/services/fileupload.js
@@ -0,0 +1,28 @@
+const aws = require('aws-sdk')
+const multer = require('multer')
+const multerS3 = require('multer-s3')
+const config = require('../config/index');
+
+aws.config.update({
+    secretAccessKey: config.aws.secretAccessKey,
+    accessKeyId: config.aws.accessKeyId,
+    region: "eu-west-3"
+})
+
+const s3 = new aws.S3()
+
+const upload = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: 'justchattingbucket',
+        acl: 'public-read',
+        metadata: function (req, file, cb) {
+            cb(null, { fieldName: "avatar" });
+        },
+        key: function (req, file, cb) {
+            cb(null, req.params.username)
+        }
+    })
+})
+
+module.exports = upload;
