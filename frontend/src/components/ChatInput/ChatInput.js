2b6d78c868a3f279e94a656469aef60e04aee5d8
Emilian Ćwiok <emilian.cw@gmail.com>
2019-06-05 15:29:49 +0200

    Add chat input

---
+++ frontend/src/components/ChatInput/ChatInput.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './ChatInput.module.scss';
+
+const ChatInput = (props) => (
+    <div className={styles.InputWrapper}>
+        <label htmlFor={props.ID} className={styles.InputLabel}>{props.children}</label>
+        <input
+            type={props.Type}
+            onChange={props.OnChange}
+            className={styles.Input}
+            placeholder={props.Placeholder}
+            id={props.ID}
+            autoComplete={props.AutoComplete}
+        />
+        <div>
+        </div>
+    </div>
+)
+
+export default ChatInput;
