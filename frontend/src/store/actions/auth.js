34e90af6e15f0126fb2ea3a8df376b3453eb594b
Emilian Ćwiok <emilian.cw@gmail.com>
2019-06-07 21:26:08 +0200

    Add default avatar

--- frontend/src/store/actions/auth.js
+++ frontend/src/store/actions/auth.js
@@ -6,6 +6,10 @@ export const signIn = data => {
         try {
             const res = await axios.post('http://localhost:3001/users/signin', data);
 
+            if (res.data.avatar === '') {
+                res.data.avatar = 'https://justchattingbucket.s3.eu-west-3.amazonaws.com/DefaultUserAvatar';
+            }
+
             dispatch({
                 type: actionTypes.AUTH_SIGN_IN,
                 payload: res.data.token,
