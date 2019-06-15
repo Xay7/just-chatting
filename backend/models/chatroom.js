b6c60c83000030496b844f05f54ac8a0e3a330e3
Emilian Ćwiok <emilian.cw@gmail.com>
2019-06-15 16:46:23 +0200

    Restructured models

--- backend/models/chatroom.js
+++ backend/models/chatroom.js
@@ -2,10 +2,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const chatSchema = new Schema({
-    id: {
-        type: String,
-        required: true
-    },
     name: {
         type: String,
         required: true,
@@ -14,38 +10,19 @@ const chatSchema = new Schema({
         type: String,
         required: true
     },
-    subscribers: [
+    members: [
         {
-            subscriber: String,
+            _id: false,
+            member: String,
             role: String,
             joined_at: String
         }
-    ],
-    channels: [
-        {
-            _id: false,
-            id: {
-                type: String
-            },
-            name: {
-                type: String
-            },
-            description: {
-                type: String
-            },
-            messages: [
-                {
-                    author: String,
-                    body: String,
-                    created_at: Date,
-                    _id: false,
-                    avatar: String
-                }
-            ]
-        }
     ]
-
-})
+}).set('toJSON', {
+    virtuals: true,
+    versionKey: false,
+    transform: function (doc, ret) { delete ret._id }
+});
 
 const chatroom = mongoose.model('chatroom', chatSchema);
 
