86b75d09f32bc26f38c7259aa8aadc428390a3e9
Emilian Ćwiok <emilian.cw@gmail.com>
2019-06-16 19:40:36 +0200

    Removed google/facebook stuff

--- backend/passport.js
+++ backend/passport.js
@@ -1,10 +1,8 @@
 const passport = require("passport");
 const JWTStrategy = require("passport-jwt").Strategy;
 const LocalStrategy = require("passport-local").Strategy;
-const { JWT_S, oauth } = require("./config/index");
+const { JWT_S } = require("./config/index");
 const User = require("./models/user");
-const GooglePlusTokenStrategy = require('passport-google-plus-token');
-const FacebookTokenStrategy = require('passport-facebook-token');
 
 const getCookie = req => {
 
@@ -43,7 +41,7 @@ passport.use(new LocalStrategy({
   async (email, password, done) => {
     try {
 
-      const user = await User.findOne({ "local.email": email });
+      const user = await User.findOne({ "email": email });
 
       if (!user) {
         return done(null, false);
@@ -63,61 +61,3 @@ passport.use(new LocalStrategy({
   }
 )
 );
-
-// Google OAUTH strategy
-
-passport.use('googleToken', new GooglePlusTokenStrategy({
-  clientID: oauth.google.clientID,
-  clientSecret: oauth.google.clientSecret,
-}, async (accessToken, refreshToken, profile, done) => {
-  try {
-    const existingUser = await User.findOne({ "google.id": profile.id });
-    if (existingUser) {
-      console.log("User arleady exists");
-      return done(null, existingUser);
-    }
-
-    console.log("User doesn't exists");
-
-    const newUser = new User({
-      method: 'google',
-      google: {
-        id: profile.id,
-        email: profile.emails[0].value
-      }
-    });
-
-    await newUser.save();
-    done(null, newUser);
-  }
-  catch (error) {
-    done(error, false, error.message);
-  }
-}));
-
-passport.use('facebookToken', new FacebookTokenStrategy({
-  clientID: oauth.facebook.clientID,
-  clientSecret: oauth.facebook.clientSecret
-}, async (accessToken, refreshToken, profile, done) => {
-  try {
-    const existingUser = await User.findOne({ "facebook.id": profile.id });
-
-    if (existingUser) {
-      return done(null, existingUser);
-    }
-
-    const newUser = new User({
-      method: 'facebook',
-      facebook: {
-        id: profile.id,
-        email: profile.emails[0].value
-      }
-    })
-    await newUser.save();
-
-    done(null, newUser);
-
-  } catch (error) {
-    done(error, false, error.message);
-  }
-}));
