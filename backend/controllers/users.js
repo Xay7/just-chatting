9934d25ea77efa64203fb9dfff7beebbb0b58907
goldentraingt <goldentraingt@gmail.com>
2021-05-26 10:02:59 +0200

    kontroler semantyka

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,114 +1,135 @@
-const JWT = require('jsonwebtoken');
-const User = require('../models/user');
-const { JWT_S } = require('../config/index');
-const upload = require('../services/fileupload');
-const uploadAvatar = upload.single('avatar');
+const JWT = require("jsonwebtoken")
+const User = require("../models/user")
+const { JWT_S } = require("../config/index")
+const upload = require("../services/fileupload")
+const uploadAvatar = upload.single("avatar")
 
-signToken = (user) => {
-  return (token = JWT.sign(
-    {
-      iss: 'HELLO',
-      sub: user.id,
-      iat: new Date().getTime(), // CURRENT TIME
-      exp: new Date().setDate(new Date().getDate() + 1), // TOMMOROW
-    },
-    JWT_S
-  ));
-};
+const signToken = (user) => {
+    return (token = JWT.sign(
+        {
+            iss: "HELLO",
+            sub: user.id,
+            iat: new Date().getTime(), // CURRENT TIME
+            exp: new Date().setDate(new Date().getDate() + 1), // TOMMOROW
+        },
+        JWT_S
+    ))
+}
 
-module.exports = {
-  signUp: async (req, res, next) => {
+const signUp = async (req, res, next) => {
     // Email and password from user
-    const { email, password, name } = req.value.body;
+    const { email, password, name } = req.value.body
     // Check if user exists in database
-    const foundUser = await User.findOne({ email: email });
-    const foundName = await User.findOne({ name: name });
+    const foundUser = await User.findOne({ email: email })
+    const foundName = await User.findOne({ name: name })
 
     if (foundUser) {
-      return res.status(403).send({ error: 'Email arleady in use' });
+        return res.status(403).send({ error: "Email arleady in use" })
     }
 
     if (foundName) {
-      return res.status(403).send({ error: 'Username arleady in use' });
+        return res.status(403).send({ error: "Username arleady in use" })
     }
 
     // Create new user
 
     const newUser = new User({
-      email: email,
-      password: password,
-      name: name,
-      avatar: 'https://justchattingbucket.s3.eu-west-3.amazonaws.com/DefaultUserAvatar',
-    });
-    await newUser.save();
+        email: email,
+        password: password,
+        name: name,
+        avatar: "https://justchattingbucket.s3.eu-west-3.amazonaws.com/DefaultUserAvatar",
+    })
+    await newUser.save()
 
-    const token = signToken(newUser);
+    const token = signToken(newUser)
 
-    res.cookie('access_token', token, {
-      httpOnly: true,
-      expires: new Date(Date.now() + 900000),
-      sameSite: 'none',
-      secure: true,
-    });
+    res.cookie("access_token", token, {
+        httpOnly: true,
+        expires: new Date(Date.now() + 900000),
+        sameSite: "none",
+        secure: true,
+    })
 
-    res.status(200).json({ success: 'User has been registered' });
-  },
-  signIn: async (req, res, next) => {
+    res.status(200).json({ success: "User has been registered" })
+}
+const signIn = async (req, res, next) => {
     if (req.session.user) {
-      return res.status(400).json({ error: 'User arleady logged in, try again later' });
+        return res
+            .status(400)
+            .json({ error: "User arleady logged in, try again later" })
     }
 
-    const avatar = req.user.avatar;
-    const username = req.user.name;
-    const id = req.user._id;
-    const token = signToken(req.user);
-    res.cookie('access_token', token, {
-      httpOnly: true,
-      expires: new Date(Date.now() + 900000),
-      sameSite: 'none',
-      secure: true,
-    });
+    const avatar = req.user.avatar
+    const username = req.user.name
+    const id = req.user._id
+    const token = signToken(req.user)
+    res.cookie("access_token", token, {
+        httpOnly: true,
+        expires: new Date(Date.now() + 900000),
+        sameSite: "none",
+        secure: true,
+    })
 
-    req.session.user = req.user;
+    req.session.user = req.user
 
     res.status(200).json({
-      username,
-      avatar,
-      id,
-    });
-  },
-  changePassword: async (req, res, next) => {
-    const password = req.body.password;
+        username,
+        avatar,
+        id,
+    })
+}
+
+const changePassword = async (req, res, next) => {
+    const password = req.body.password
     await User.findOne({ _id: req.params.id }, function (err, doc) {
-      doc.password = password;
-      doc.save();
-    });
+        doc.password = password
+        doc.save()
+    })
 
-    res.status(200).json({ success: 'Password has been changed' });
-  },
-  changeAvatar: async (req, res, next) => {
-    const id = req.params.id;
+    res.status(200).json({ success: "Password has been changed" })
+}
 
-    await User.findOneAndUpdate({ _id: id }, { avatar: `https://justchattingbucket.s3.eu-west-3.amazonaws.com/${id}` });
+const changeAvatar = async (req, res, next) => {
+    const id = req.params.id
+
+    await User.findOneAndUpdate(
+        { _id: id },
+        {
+            avatar: `https://justchattingbucket.s3.eu-west-3.amazonaws.com/${id}`,
+        }
+    )
 
     await uploadAvatar(req, res, function (err) {
-      if (err) {
-        return res.status(403).json({ error: err.message });
-      }
+        if (err) {
+            return res.status(403).json({ error: err.message })
+        }
 
-      res.status(201).json({ success: 'Updated avatar' });
-    });
-  },
-  logout: async (req, res, next) => {
-    req.session.destroy();
-    res.status(200).json({ success: 'Logout successful' });
-  },
-  googleOAuth: async (req, res, next) => {
-    const token = signToken(req.user);
-    res.status(200).json({ token });
-  },
-  facebookOAuth: async (req, res, next) => {
-    const token = signToken(req.user);
-    res.status(200).json({ token });
-  },
-};
+        res.status(201).json({ success: "Updated avatar" })
+    })
+}
+
+const logout = async (req, res, next) => {
+    req.session.destroy()
+    res.status(200).json({ success: "Logout successful" })
+}
+
+const googleOAuth = async (req, res, next) => {
+    const token = signToken(req.user)
+    res.status(200).json({ token })
+}
+
+const facebookOAuth = async (req, res, next) => {
+    const token = signToken(req.user)
+    res.status(200).json({ token })
+}
+
+module.exports = {
+    signToken,
+    signUp,
+    signIn,
+    changePassword,
+    changeAvatar,
+    logout,
+    googleOAuth,
+    facebookOAuth,
+}
--- backend/index.js
+++ backend/index.js
@@ -1,45 +1,53 @@
-const express = require('express');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const app = express();
-const sessionSecret = require('./config/index').session;
-const cookieParser = require('cookie-parser');
-const socket = require('socket.io');
-const expressSession = require('express-session');
-const port = process.env.PORT || 3001;
-require('dotenv').config({ path: __dirname + '/.env' });
+const express = require("express")
+const morgan = require("morgan")
+const bodyParser = require("body-parser")
+const cors = require("cors")
+const mongoose = require("mongoose")
+const app = express()
+const sessionSecret = require("./config/index").session
+const cookieParser = require("cookie-parser")
+const socket = require("socket.io")
+const expressSession = require("express-session")
+const port = process.env.PORT || 3001
+require("dotenv").config({ path: __dirname + "/.env" })
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.set('useFindAndModify', false);
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+mongoose.set("useFindAndModify", false)
 
-const server = app.listen(port);
-io = socket(server);
+const server = app.listen(port)
+io = socket(server)
 
-module.exports = io;
+module.exports = io
 
-app.use(morgan('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
+app.use(morgan("dev"))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
 app.use(
-  cors({
-    origin: 'http://localhost:3000',
-    credentials: true,
-  })
-);
+    cors({
+        origin: "http://localhost:3000",
+        credentials: true,
+    })
+)
 app.use(
-  expressSession({
-    secret: sessionSecret,
-    resave: false,
-    saveUninitialized: false,
-    cookie: { maxAge: 1800000, sameSite: 'none', secure: true, httpOnly: true },
-  })
-);
+    expressSession({
+        secret: sessionSecret,
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            maxAge: 1800000,
+            sameSite: "none",
+            secure: true,
+            httpOnly: true,
+        },
+    })
+)
 
-app.use('/users', require('./routes/users'));
-app.use('/chatrooms', require('./routes/chatroom'));
-app.use('/channels', require('./routes/channel'));
+app.use("/users", require("./routes/users"))
+app.use("/chatrooms", require("./routes/chatroom"))
+app.use("/channels", require("./routes/channel"))
 
-console.log(`Listening at ${port}`);
+console.log(`Listening at ${port}`)
